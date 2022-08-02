3a27782c122206cfc25ff78cc45347fe4fc24786
Tom Oram <tom.oram@armakuni.com>
2022-08-02 15:14:30 +0100

    chore: Remove exporatory test code
    Refs: BLAIS5-3209

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from flask import Request
 from google.cloud.logging_v2.handlers import StructuredLogHandler, setup_logging
 
 from lib import slack_alerts
-from lib.slack import SlackMessage
 from lib.slack.alerter import create_slack_alerter
 
 setup_logging(StructuredLogHandler())
@@ -22,21 +21,3 @@ def send_slack_alert(event: dict, _context) -> str:
 def log_error(_request: Request) -> str:
     logging.error("Example error message", extra=dict(reason="proof_of_concept"))
     return "Error logged"
-
-
-def send_to_slack() -> None:
-    slack_url = os.environ["SLACK_URL"]
-    send_alert = create_slack_alerter(slack_url)
-
-    send_alert(
-        SlackMessage(
-            title="ERROR: Example error message where something went wrong",
-            fields=dict(Platform="gce_instance", Application="bts"),
-            content="Message content goes here",
-            footnote="Actions go here",
-        )
-    )
-
-
-if __name__ == "__main__":
-    send_to_slack()
