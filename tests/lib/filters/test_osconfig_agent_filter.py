2f882bc70c5785d6e17c3df976bec78532e637f8
enigma-cloud <57638182+Enigma-cloud@users.noreply.github.com>
2023-03-22 14:07:40 +0000

    refactor: use existing pattern for mocking log entries in tests

--- tests/lib/filters/test_osconfig_agent_filter.py
+++ tests/lib/filters/test_osconfig_agent_filter.py
@@ -1,21 +1,20 @@
+import pytest
 import datetime
+import dataclasses
 
 from lib.log_processor import ProcessedLogEntry
 from lib.filters.osconfig_agent_filter import osconfig_agent_filter
 
 
-def mock_log_entry(
-    platform="gce_instance",
-    data=dict(
-        description="2023-02-25T03:46:49.1619Z OSConfigAgent Error main.go:231: unexpected end of JSON input\r\n"
-    ),
-) -> ProcessedLogEntry:
-
+@pytest.fixture()
+def processed_log_entry() -> ProcessedLogEntry:
     return ProcessedLogEntry(
         message="OSConfigAgent Error main.go:231: unexpected end of JSON input",
-        data=data,
+        data=dict(
+            description="2023-02-25T03:46:49.1619Z OSConfigAgent Error main.go:231: unexpected end of JSON input\r\n"
+        ),
         severity="ERROR",
-        platform=platform,
+        platform="gce_instance",
         application="blaise-gusty-data-entry-1",
         log_name="/logs/gce-example",
         timestamp=datetime.datetime(2023, 2, 25, 3, 46, 57, 99633),
@@ -26,52 +25,60 @@ def mock_log_entry(
     )
 
 
-def test_log_is_from_gce_instance():
-    instance = mock_log_entry()
-    log_is_skipped = osconfig_agent_filter(instance)
+def test_log_is_from_gce_instance(processed_log_entry: ProcessedLogEntry):
+    log_is_skipped = osconfig_agent_filter(processed_log_entry)
 
     assert log_is_skipped == True
 
 
-def test_log_is_not_from_gce_instance():
-    instance = mock_log_entry(platform="not_gce_instance")
-    log_is_skipped = osconfig_agent_filter(instance)
-
-    assert log_is_skipped == False
-
-
-def test_log_data_is_dict_and_has_description():
-    instance = mock_log_entry()
-    log_is_skipped = osconfig_agent_filter(instance)
-
-    assert log_is_skipped == True
-
-
-def test_log_data_is_dict_but_no_description():
-    instance = mock_log_entry(data=dict(source_name="gcp"))
-    log_is_skipped = osconfig_agent_filter(instance)
-
-    assert log_is_skipped == False
-
-
-def test_log_data_is_not_dict_and_no_description():
-    instance = mock_log_entry(data="no-relevant-data")
-    log_is_skipped = osconfig_agent_filter(instance)
-
-    assert log_is_skipped == False
-
-
-def test_log_data_description_has_target_text():
-    instance = mock_log_entry()
-    log_is_skipped = osconfig_agent_filter(instance)
-
-    assert log_is_skipped == True
-
-
-def test_log_data_description_has_no_target_text():
-    instance = mock_log_entry(
-        data=dict(description="ERROR: there is no relevant data descrtiption")
+def test_log_is_not_from_gce_instance(processed_log_entry: ProcessedLogEntry):
+    processed_log_entry = dataclasses.replace(
+        processed_log_entry, platform="not_gce_instance"
     )
-    log_is_skipped = osconfig_agent_filter(instance)
+    log_is_skipped = osconfig_agent_filter(processed_log_entry)
+
+    assert log_is_skipped == False
+
+
+def test_log_data_is_dict_and_has_description(processed_log_entry: ProcessedLogEntry):
+    log_is_skipped = osconfig_agent_filter(processed_log_entry)
+
+    assert log_is_skipped == True
+
+
+def test_log_data_is_dict_but_no_description(processed_log_entry: ProcessedLogEntry):
+    processed_log_entry = dataclasses.replace(
+        processed_log_entry, data=dict(source_name="gcp")
+    )
+    log_is_skipped = osconfig_agent_filter(processed_log_entry)
+
+    assert log_is_skipped == False
+
+
+def test_log_data_is_not_dict_and_no_description(
+    processed_log_entry: ProcessedLogEntry,
+):
+    processed_log_entry = dataclasses.replace(
+        processed_log_entry, data="no-relevant-data"
+    )
+    log_is_skipped = osconfig_agent_filter(processed_log_entry)
+
+    assert log_is_skipped == False
+
+
+def test_log_data_description_has_target_text(processed_log_entry: ProcessedLogEntry):
+    log_is_skipped = osconfig_agent_filter(processed_log_entry)
+
+    assert log_is_skipped == True
+
+
+def test_log_data_description_has_no_target_text(
+    processed_log_entry: ProcessedLogEntry,
+):
+    processed_log_entry = dataclasses.replace(
+        processed_log_entry,
+        data=dict(description="ERROR: there is no relevant data descrtiption"),
+    )
+    log_is_skipped = osconfig_agent_filter(processed_log_entry)
 
     assert log_is_skipped == False
