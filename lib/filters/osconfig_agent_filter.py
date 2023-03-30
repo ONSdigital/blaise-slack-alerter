913f85831e58d58aefa31cc0871a006d531ea988
enigma-cloud <57638182+Enigma-cloud@users.noreply.github.com>
2023-03-30 17:18:30 +0100

    feat: temporarily silence OSConfig context deadline exceeded errors

--- lib/filters/osconfig_agent_filter.py
+++ lib/filters/osconfig_agent_filter.py
@@ -3,22 +3,25 @@ from lib.log_processor import ProcessedLogEntry
 
 def osconfig_agent_filter(log_entry: ProcessedLogEntry):
     entry_data = log_entry.data
+    print(log_entry)
 
     if log_entry.platform != "gce_instance":
         return False
 
-    if (
+    # TODO: Remove after BLAIS5-3705 concludes
+    if (log_entry.message and "context deadline exceeded" in log_entry.message) or (
         type(entry_data) is dict
         and "description" in entry_data
         and "OSConfigAgent Error" in entry_data["description"]
-        and "unexpected end of JSON input" in entry_data["description"]
+        and "context deadline exceeded" in entry_data["description"]
     ):
         return True
 
     if (
         type(entry_data) is dict
         and "description" in entry_data
-        and "context deadline exceeded" in entry_data["description"]
+        and "OSConfigAgent Error" in entry_data["description"]
+        and "unexpected end of JSON input" in entry_data["description"]
     ):
         return True
 
