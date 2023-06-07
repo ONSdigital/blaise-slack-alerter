945d170ada1082a6c0e97be660020fa63347df7b
enigma-cloud <57638182+Enigma-cloud@users.noreply.github.com>
2023-06-07 11:49:52 +0100

    refactor: break filter logic into small if statements

--- lib/filters/agent_connect_filter.py
+++ lib/filters/agent_connect_filter.py
@@ -7,11 +7,13 @@ def agent_connect_filter(log_entry: ProcessedLogEntry) -> bool:
     if log_entry.platform != "gce_instance":
         return False
 
-    if (
-        type(entry_data) is dict
-        and "description" in entry_data
-        and "Agent connect error" in entry_data["description"]
-    ):
-        return True
+    if not isinstance(entry_data, dict) or "description" not in entry_data:
+        return False
 
-    return False
+    if (
+        "Agent connect error: The HTTP request timed out after 00:01:00.. Retrying until reconnected."
+        not in entry_data["description"]
+    ):
+        return False
+
+    return True
