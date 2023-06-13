242aa70ccb221453ddeb62f557b144b4bbb0222f
James Anthony Williams <81305008+JAWilliamsONS@users.noreply.github.com>
2023-06-13 09:47:17 +0100

    chore: remove comments and format

--- lib/filters/osconfig_agent_filter.py
+++ lib/filters/osconfig_agent_filter.py
@@ -1,42 +1,22 @@
 from lib.log_processor import ProcessedLogEntry
 
-# DISCLAIMER: Not sure if this is the best way to do it but it seems to work. Feel free to change it if you think there's a better way.
 
 def osconfig_agent_filter(log_entry: ProcessedLogEntry):
-    # NOTE: I put these conditionals to satisfy the type checker
     if not isinstance(log_entry.platform, str):
         return False
 
-    # NOTE: Not even sure if we really need to check for this but I included it just in case
     if log_entry.platform != "gce_instance":
         return False
 
-    # NOTE: I put these conditionals to satisfy the type checker
     if not isinstance(log_entry.message, str):
         return False
 
-    # NOTE: This text seems to be common across the OSConfigAgent logs and can be found in log_entry.message and depending on the log
-    # , it can be found within log_entry.log_name and log_entry.data["description"]
     if "unexpected end of JSON input" not in log_entry.message:
         return False
 
-    # NOTE: I put these conditionals to satisfy the type checker
     if not isinstance(log_entry.log_name, str):
         return False
 
-    # NOTE: If the log passes through through the previous conditionals,
-    # then we can assume that at this point that it's a log with "unexpected end of JSON input"
-    # but we still don't know whether it is definitely an OSConfigAgent log
-    # NOTE: So to confirm that it is an OSConfigAgent log,
-    # we check:
-    # if the log_entry.log_name contains "OSConfigAgent"
-    # or if the log_entry.message contains "OSConfigAgent Error"
-    # NOTE: In this conditional, if take the OSConfigAgent Error log as an example and go through this.
-    # We should find that the text "OSConfigAgent Error" is in the log_entry.message but not in log_entry.log_name
-    # (we get something else, e.g. projects/ons-blaise-v2-prod/logs/winevt.raw)
-    # If so the overall statement should evaluate to False as {True and False} is False,
-    # which means at this point we ultimately pass over all of the conditionals and return True. With how we've set up the filters,
-    # returning True from a filter function means that the log is skipped and returning False means that the log is sent.
     if (
         "OSConfigAgent Error" not in log_entry.message
         and "OSConfigAgent" not in log_entry.log_name
