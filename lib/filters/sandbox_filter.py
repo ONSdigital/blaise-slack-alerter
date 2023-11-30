f49c0432016ef066d9f1806159d1599a00eb0083
elthorne <46898565+elthorne@users.noreply.github.com>
2023-11-30 10:56:46 +0000

    feat: wip - silence all sandbox alerts
    refs: BLAIS5-3953

--- lib/filters/sandbox_filter.py
+++ lib/filters/sandbox_filter.py
@@ -1,10 +1,25 @@
 from lib.log_processor import ProcessedLogEntry
 
 
+def is_sandbox_environment(project_name):
+    formal_environments = [
+        # "ons-blaise-v2-dev",
+        "ons-blaise-v2-dev-training",
+        "ons-blaise-v2-preprod",
+        "ons-blaise-v2-prod",
+    ]
+
+    if any(match in project_name for match in formal_environments):
+        return False
+
+    return True
+
+
 def sandbox_filter(log_entry: ProcessedLogEntry) -> bool:
     if not log_entry.log_name:
         return False
 
-    if "ons-blaise-v2-dev-jw09" in log_entry.log_name:
+    if is_sandbox_environment(log_entry.log_name):
         return True
+
     return False
--- tests/lib/filters/test_sandbox_filter.py
+++ tests/lib/filters/test_sandbox_filter.py
@@ -2,35 +2,45 @@ import pytest
 import datetime
 
 from lib.log_processor import ProcessedLogEntry
-from lib.filters.sandbox_filter import sandbox_filter
+from lib.filters.sandbox_filter import sandbox_filter, is_sandbox_environment
 
 
 @pytest.fixture()
 def processed_log_entry() -> ProcessedLogEntry:
     return ProcessedLogEntry(
-        message='Successfully collected profile HEAP.',
+        message="This is an example message",
         data={
-            'logName': 'projects/ons-blaise-v2-dev-jw09/logs/%40google-cloud%2Fprofiler',
-            'resource': {
-                'type': 'gae_app',
-                'labels': {
-                    'version_id': '20231129t144628',
-                    'module_id': 'dqs-ui',
-                    'zone': 'europe-west2-1'
-                }
+            "logName": "projects/ons-blaise-v2-dev-jw09/logs/%40google-cloud%2Fprofiler",
+            "resource": {
+                "type": "gae_app_example",
+                "labels": {
+                    "version_id": "20231129t144628_example",
+                    "module_id": "dqs-ui-example",
+                    "zone": "europe-west2-1-example",
+                },
             },
-            'timestamp': '2023-11-29T15:34:54.591Z'
+            "timestamp": "2023-11-29T15:34:54.591Z",
         },
-        severity='DEBUG',
-        platform='gae_app',
-        application='dqs-ui',
-        log_name='projects/ons-blaise-v2-dev-jw09/logs/stdout',
+        severity="DEBUG",
+        platform="gae_app_example",
+        application="dqs-ui-example",
+        log_name="projects/ons-blaise-v2-dev-jw09/logs/stdout",
         timestamp=datetime.datetime(2023, 11, 29, 15, 34, 54, 591),
         log_query={
-            'resource.type': 'gae_app',
-            'resource.labels.module_id': 'dqs-ui'
+            "resource.type": "gae_app_example",
+            "resource.labels.module_id": "dqs-ui-example",
         },
-        most_important_values=['status', 'host', 'method', 'resource', 'ip', 'latency', 'responseSize', 'httpVersion'])
+        most_important_values=[
+            "status",
+            "host",
+            "method",
+            "resource",
+            "ip",
+            "latency",
+            "responseSize",
+            "httpVersion",
+        ],
+    )
 
 
 def test_log_is_skipped_for_sandbox_environment(
@@ -41,3 +51,33 @@ def test_log_is_skipped_for_sandbox_environment(
 
     # assert
     assert log_is_skipped is True
+
+
+def test_is_sandbox_environment_returns_true_for_sandbox_environment():
+    # arrange
+    log_name_example = "projects/ons-blaise-v2-dev-jw09/logs/stdout"
+
+    # act
+    result = is_sandbox_environment(log_name_example)
+
+    # assert
+    assert result is True
+
+
+@pytest.mark.parametrize(
+    "formal_log_name_example",
+    [
+        # "projects/ons-blaise-v2-dev/logs/stdout",
+        "projects/ons-blaise-v2-dev-training/logs/stdout",
+        "projects/ons-blaise-v2-preprod/logs/stdout",
+        "projects/ons-blaise-v2-prod/logs/stdout",
+    ],
+)
+def test_is_sandbox_environment_returns_false_for_formal_environment(
+    formal_log_name_example,
+):
+    # act
+    result = is_sandbox_environment(formal_log_name_example)
+
+    # assert
+    assert result is False
