6c560ea745cc77b7d4a40759c46c660ac8e4c299
lambeb <benjamin.lambe@ons.gov.uk>
2024-05-22 10:54:25 +0100

    Added some code to check whether keeping track of a last_sent_time will allow us to only restrict the alert running once

--- lib/filters/invalid_login_attempt_filter.py
+++ lib/filters/invalid_login_attempt_filter.py
@@ -1,9 +1,14 @@
 import logging
-
+from datetime import datetime, timedelta
 from lib.log_processor import ProcessedLogEntry
+
+last_sent_alert = None
 
 
 def invalid_login_attempt_filter(log_entry: ProcessedLogEntry) -> bool:
+
+    global last_sent_alert
+    timeout = 5
 
     if not isinstance(log_entry.severity, str):
         return False
@@ -17,6 +22,13 @@ def invalid_login_attempt_filter(log_entry: ProcessedLogEntry) -> bool:
     if 'Required "container.clusters.list" permission(s)' not in log_entry.message:
         return False
 
-    logging.info(f"Skipping invalid login attempt alert")
+    if last_sent_alert is None:
+        last_sent_alert = datetime.now()
+        return False
 
+    if last_sent_alert <= datetime.now() - timedelta(minutes=timeout):
+        last_sent_alert = datetime.now()
+        return False
+
+    logging.info(f"Skipping invalid login attempt alert")
     return True
