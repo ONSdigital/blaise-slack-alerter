28bc21131f6df8f748cac04852c76054c12aff0c
MD Maruf Motaleb <md.maruf.motaleb@ons.gov.uk>
2024-09-24 17:27:05 +0100

    linting

--- lib/filters/paramiko_filter.py
+++ lib/filters/paramiko_filter.py
@@ -2,20 +2,21 @@ import logging
 
 from lib.log_processor import ProcessedLogEntry
 
+
 def paramiko_filter(log_entry: ProcessedLogEntry) -> bool:
-    
+
     if not isinstance(log_entry.severity, str):
         return False
-    
+
     if not isinstance(log_entry.message, str):
         return False
-    
+
     if log_entry.severity != "ERROR":
         return False
-    
+
     if log_entry.platform != "cloud_run_revision":
         return False
-    
+
     if ("paramiko") not in log_entry.message:
         return False
 
