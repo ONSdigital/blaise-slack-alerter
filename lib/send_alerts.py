02c357d9513700dc0cfcd120cb181023f05572c2
Elinor <46898565+elthorne@users.noreply.github.com>
2024-12-17 09:59:30 +0000

    chore: remove all filters for testing
    refs: BLAIS5-4571

--- lib/send_alerts.py
+++ lib/send_alerts.py
@@ -5,51 +5,15 @@ from typing import List
 from lib.alerter import Alerter
 from lib.cloud_run_revision import InvalidCloudRunRevisionEvent, parse_event
 from lib.cloud_logging import parse_log_entry
-from lib.filters.all_preprod_and_training_alerts_except_erroneous_questionnaire_filter import (
-    all_preprod_and_training_alerts_except_erroneous_questionnaire_filter,
-)
-from lib.filters.auditlog_filter import auditlog_filter
-from lib.filters.agent_connect_filter import agent_connect_filter
-from lib.filters.osconfig_agent_filter import osconfig_agent_filter
-from lib.filters.ip_space_exhausted_filter import ip_space_exhausted_filter
-from lib.filters.rproxy_lookupEffectiveGuestPolicies_filter import (
-    rproxy_lookupEffectiveGuestPolicies_filter,
-)
-from lib.filters.watching_metadata_invalid_character_filter import (
-    watching_metadata_invalid_character_filter,
-)
 from lib.log_processor import (
     ProcessedLogEntry,
     CreateAppLogPayloadFromLogEntry,
 )
 from lib.log_processor import process_log_entry
-from lib.filters.no_instance_filter import no_instance_filter
-from lib.filters.invalid_login_attempt_filter import invalid_login_attempt_filter
-from lib.filters.requested_entity_was_not_found_filter import (
-    requested_entity_was_not_found_filter,
-)
-from lib.filters.execute_sql_filter import execute_sql_filter
-from lib.filters.paramiko_filter import paramiko_filter
-from lib.filters.bootstrapper_filter import bootstrapper_filter
-from lib.filters.generic_not_found_filter import generic_not_found_filter
 
 
 def log_entry_skipped(log_entry: ProcessedLogEntry):
     filters = [
-        all_preprod_and_training_alerts_except_erroneous_questionnaire_filter,
-        osconfig_agent_filter,
-        auditlog_filter,
-        agent_connect_filter,
-        rproxy_lookupEffectiveGuestPolicies_filter,
-        watching_metadata_invalid_character_filter,
-        ip_space_exhausted_filter,
-        no_instance_filter,
-        invalid_login_attempt_filter,
-        requested_entity_was_not_found_filter,
-        execute_sql_filter,
-        paramiko_filter,
-        bootstrapper_filter,
-        generic_not_found_filter,
     ]
 
     for filter in filters:
