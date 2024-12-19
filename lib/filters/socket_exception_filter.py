0a35defca4d5f887ce928c26b605bd0d8af6323c
motalm <md.maruf.motaleb@ons.gov.uk>
2024-12-19 15:05:04 +0000

    added few files (nothing tested yet)

---
+++ lib/filters/socket_exception_filter.py
@@ -0,0 +1,25 @@
+import logging
+from lib.log_processor import ProcessedLogEntry
+
+
+def socket_exception_filter(log_entry: ProcessedLogEntry) -> bool:
+    return ""
+    '''
+    if not isinstance(log_entry.severity, str):
+        return False
+
+    if not isinstance(log_entry.message, str):
+        return False
+
+    if log_entry.severity != "ERROR":
+        return False
+
+    if (
+        'generic::not_found: Failed to fetch "latest' not in log_entry.message
+        and 'generic::not_found: Failed to fetch "version_' not in log_entry.message
+    ):
+        return False
+
+    logging.info(f"Skipping generic not found alert")
+    return True
+   '''
